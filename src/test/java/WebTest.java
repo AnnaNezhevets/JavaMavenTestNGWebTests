b669d3fd4d950ec84e55cb3c07358d88062b378f
AnnaNezhevets <nuta2009kr@gmail.com>
2022-05-22 15:44:49 +0300

    second test

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -40,4 +40,24 @@ public class WebTest {
 
     }
 
+    @Test
+    public void testStartPage(){
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C:/Users/Пользователь/Downloads/chromedriver_win32/chromedriver.exe";
+        String url = "https://www.99-bottles-of-beer.net/";
+        String expectedResult = "99 Bottles of Beer";
+
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+
+        WebElement nameWebSite = driver.findElement(By.xpath("//body/div[@id ='wrap']/div[@id = 'header']/h1"));
+        String actualResult = nameWebSite.getText();
+
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.quit();
+    }
+
 }
